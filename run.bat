16490e1b01a90381307f850e8662c70e63f5e841
amolkap25 <amol.kapade90@gmail.com>
2024-06-06 11:08:28 +0530

    Add run.bat file

---
+++ run.bat
@@ -0,0 +1,3 @@
+cd C:\Users\HP\OneDrive\Desktop\Selenium_workspace_jenkins_demo
+java  C:\Users\HP\OneDrive\Desktop\Selenium_workspace_jenkins_demo\lib\*;C:\Users\HP\OneDrive\Desktop\Selenium_workspace_jenkins_demo\target\test-classes\com\amol\automation\testcase\* org.testng.TestNG testng.xml
+pause
