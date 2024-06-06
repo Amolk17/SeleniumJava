7bf57eb8872d06c29e6f47cbdf75ac2f7908e49e
amolkap25 <amol.kapade90@gmail.com>
2024-06-06 17:51:51 +0530

    reset amol

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 cd C:\Users\HP\OneDrive\Desktop\Selenium_workspace_jenkins_demo
 java  C:\Users\HP\OneDrive\Desktop\Selenium_workspace_jenkins_demo\lib\*;C:\Users\HP\OneDrive\Desktop\Selenium_workspace_jenkins_demo\target\test-classes\com\amol\automation\testcase\* org.testng.TestNG testng.xml
 pause
+
+for git purpose
+
+git commit 1
